84e3253e5acb9e81ed03756cbae9840c07e04e5f
MacroUniverse <270174408@qq.com>
2022-10-07 03:00:28 -0500

    windows test successful!

--- make-windows.sh
+++ make-windows.sh
@@ -1,1 +1,1 @@
-g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I ../../../include/ -I windows/include -L ../../../lib -L windows/bin -o main.so main.cpp -l arb -l flint -l libbex
+g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I ../../../include/ -I windows/include -L ../../../lib -L windows/bin -o main.dll main.cpp -l arb-2 -l flint -l libbex
