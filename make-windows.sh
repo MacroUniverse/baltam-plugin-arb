4900187cbc5084176a69fcf6ae188ba33237cec0
MacroUniverse <270174408@qq.com>
2022-10-07 03:08:33 -0500

    update

--- make-windows.sh
+++ make-windows.sh
@@ -1,1 +1,2 @@
 g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I ../../../include/ -I windows/include -L ../../../lib -L windows/bin -o main.dll main.cpp -l arb-2 -l flint -l libbex
+cp windows/bin/* ../../../../lib/
--- make.sh
+++ make.sh
@@ -1,1 +1,2 @@
 g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I ../../../include/ -o main.so main.cpp  -l flint-arb
+cp bin/* ../../../../lib/
