35fbb8e2453c7621dc744325ec12fd1a78ffc75a
MacroUniverse <270174408@qq.com>
2022-10-07 03:22:59 -0500

    update

--- make-windows.sh
+++ make-windows.sh
@@ -1,4 +1,4 @@
 g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I ../../../include/ -I windows/include -L ../../../lib -L windows/bin -o main.dll main.cpp -l arb-2 -l flint -l libbex
-mkdir ../../../../plugins/arb
+mkdir -p ../../../../plugins/arb
 cp main.so ../../../../plugins/arb
 cp windows/bin/* ../../../../lib/
