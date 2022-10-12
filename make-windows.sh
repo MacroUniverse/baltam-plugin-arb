6555da80acd2107295b871acca6c15c7f01408e1
MacroUniverse <270174408@qq.com>
2022-10-12 01:22:36 -0500

    make this repo position independent

--- make-windows.sh
+++ make-windows.sh
@@ -1,1 +1,1 @@
-g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I /c/baltamatica/bex/include -I windows/include -L /c/baltamatica/bex/lib -L windows/bin-arb4win -o main.dll main.cpp -l arb-2 -l mpfr-6 -l flint -l gmp-23 -l bex
+g++ -shared -fPIC -DBV_USE_DYN_LOADER -D SLS_USE_ARB -I /c/baltamatica/ -I /c/baltamatica/bex/include -I windows/include -L /c/baltamatica/bex/lib -L windows/bin-arb4win -o main.dll main.cpp -l arb-2 -l mpfr-6 -l flint -l gmp-23 -l bex
